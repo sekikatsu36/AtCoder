410950b9036fb9cd4c689d409665da68355f1429
sekikatsu <22820631+sekikatsu36@users.noreply.github.com>
2019-09-28 15:01:19 +0900

    提出版

--- AtCoder/Abc/QuestionA.cs
+++ AtCoder/Abc/QuestionA.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AtCoder.Abc
 {
-    //https://atcoder.jp/contests/abc141/tasks/abc141_a
     public class QuestionA
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hoge");
+            // 整数の入力
+            int n = int.Parse(Console.ReadLine());
+
+            // 整数配列の入力
+            var aArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i));
+            var sortedArray = aArray.OrderBy(a => a);
+
+            long result = long.MaxValue;
+            for(int i = 0; i < n; i++)
+            {
+                int tmp = sortedArray.ElementAt(i + n) - sortedArray.ElementAt(i);
+                if(tmp < result)
+                {
+                    result = tmp;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
--- AtCoder/Abc/QuestionB.cs
+++ AtCoder/Abc/QuestionB.cs
@@ -10,6 +10,72 @@ namespace AtCoder.Abc
     {
         public static void Main(string[] args)
         {
+            // 整数の入力
+            int n = int.Parse(Console.ReadLine());
+            // 文字列の入力
+            string s = Console.ReadLine();
+
+            bool[] enables = new bool[n+1]; //0は欠番
+
+            int li = 1; //次に左から来た人が座る位置(＝まだ空いてる位置)
+            int ri = n; //次に右から来た人が座る位置(＝まだ空いてる位置)
+            bool flag = true;
+
+            foreach(char c in s.ToArray())
+            {
+                if (flag && ri < li)
+                {
+                    //1週目が埋まり切ったので、2週目開始
+                    flag = false;
+                    li = 1;
+                    ri = n;
+                }
+
+                if (c == 'L')
+                {
+                    if(flag)
+                    {
+                        Console.WriteLine(li);
+                        enables[li] = true;
+                        li += 2;
+                    }
+                    else
+                    {
+                        for(int i = li; i <= n; i++)
+                        {
+                            if(enables[i] == false)
+                            {
+                                Console.WriteLine(i);
+                                enables[i] = true;
+                                li = i;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (flag)
+                    {
+                        Console.WriteLine(ri);
+                        enables[ri] = true;
+                        ri -= 2;
+                    }
+                    else
+                    {
+                        for (int i = ri; i >= 0; i--)
+                        {
+                            if (enables[i] == false)
+                            {
+                                Console.WriteLine(i);
+                                enables[i] = true;
+                                ri = i;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
