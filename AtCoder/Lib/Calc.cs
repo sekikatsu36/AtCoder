2e3ee65a3dffa3aad1c92602449488aab3670356
sekikatsu <22820631+sekikatsu36@users.noreply.github.com>
2020-02-09 22:50:24 +0900

    nCr/nPr追加

--- AtCoder/Lib/Calc.cs
+++ AtCoder/Lib/Calc.cs
@@ -15,5 +15,38 @@ namespace AtCoder.Lib
         {
             return Convert.ToInt32(Math.Ceiling((double)x / a));
         }
+
+        /// <summary>
+        /// nCrを求める
+        /// </summary>
+        public static long nCr(int n, int r)
+        {
+            // naive: return Factorial(n) / (Factorial(r) * Factorial(n - r));
+            return nPr(n, r) / Factorial(r);
+        }
+
+        /// <summary>
+        /// nPrを求める
+        /// </summary>
+        public static long nPr(int n, int r)
+        {
+            // naive: return Factorial(n) / Factorial(n - r);
+            return FactorialDivision(n, n - r);
+        }
+
+        private static long FactorialDivision(int topFactorial, int divisorFactorial)
+        {
+            long result = 1;
+            for (int i = topFactorial; i > divisorFactorial; i--)
+                result *= i;
+            return result;
+        }
+
+        private static long Factorial(int i)
+        {
+            if (i <= 1)
+                return 1;
+            return i * Factorial(i - 1);
+        }
     }
 }
